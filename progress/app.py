d7fa037e4ddebefdec549b11ea8ae8114ac19fcb
alexmagwe <alexmagwe@gmail.com>
2020-07-20 18:37:13 +0000

    changed dependencies

--- progress/app.py
+++ progress/app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,url_for,request,jsonify
 import os
-from flask_script import Manager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_socketio import SocketIO
