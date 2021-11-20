f8347ce75815ca0683a914b3813b20a25cf05b0a
alexmagwe <alexmagwe@gmail.com>
2021-11-20 14:13:55 +0300

    changed layout to grid

--- progress/config.py
+++ progress/config.py
@@ -10,7 +10,6 @@ class Development:
     SQLALCHEMY_DATABASE_URI=environ.get('DATABASE_URL')
     SQLALCHEMY_COMMIT_ON_TEARDOWN=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    FLASK_APP='app.py'
     DEBUG=True
     SSL_REDIRECT=True
 class Production(object):
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
-from progress.main import app
+from .progress.main import app
 if __name__ == '__main__':
     app.run()
